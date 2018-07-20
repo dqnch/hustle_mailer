0556b58951ba4604fba1c9c7fe0d16162df22adf
kawamura.hryk <kawamura.hryk@gmail.com>
2018-07-20 11:08:00 +0900

    Fix initializer

--- config/initializers/hustle_mailer.rb
+++ config/initializers/hustle_mailer.rb
@@ -1,11 +1,11 @@
-unless Rails.env.production?
-  ActionMailer::Base.register_interceptor(SubjectInterceptor)
-end
-
 module HustleMailer
   class SubjectInterceptor
     def self.delivering_email(message)
       message.subject.isert(0, "[#{Rails.env}] ")
     end
   end
+end
+
+unless Rails.env.production?
+  ActionMailer::Base.register_interceptor(HustleMailer::SubjectInterceptor)
 end
